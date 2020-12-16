cd3cfa0068aea929fbb714bf50a57f83ea1d00e5
Marco Mangan <marco.mangan@gmail.com>
2020-12-16 19:51:33 -0300

    #4 #5

--- dyrapy/datasets/_base.py
+++ dyrapy/datasets/_base.py
@@ -2,19 +2,47 @@
 Base IO code for all datasets
 """
 
+
 # Copyright (c) 2020 Marco Mangan <marco.mangan@gmail.com>
 # License: BSD 3 clause
+import pandas
 
 def load_ouvidoria():
-	"""Load and return the dadosabertos.poa.br ouvidoria dataset.
-	
-	The ouvidoria dataset is a public open dataset from Prefeitura de Porto Alegre, Rio Grande do Sul, Brazil. 	 
-	
-	Examples
-	--------
-	>>> from dyrapy.datasets import load_ouvidoria
-	>>> data = load_ouvidoria()
-	>>> data.target.head()
-	
-	"""
-	pass
+    """Load and return the dadosabertos.poa.br ouvidoria dataset.
+
+    The ouvidoria dataset is a public open dataset from Prefeitura de Porto Alegre, Rio Grande do Sul, Brazil.
+
+    Examples
+    --------
+    >>> from dyrapy.datasets import load_ouvidoria
+    >>> data = load_ouvidoria()
+    >>> data.head()
+
+    """
+    data = pandas.read_csv('../dyrapy/datasets/data/ouvidoria.csv', sep=';', na_values=".")
+
+    print('Data head...')
+    print(data.head())
+
+    print('Data describe..')
+    print(data.describe())
+
+    print("Recoding categories...")
+    data.descricao = data.descricao.astype('category')
+    data.sigla = data.sigla.astype('category')
+    data.statussolicitacao = data.statussolicitacao.astype('category')
+    data.tipofim = data.tipofim.astype('category')
+
+    print('Recodind dates..')
+    data.data_extracao = pandas.to_datetime(data.data_extracao)
+    data.dataregistro = pandas.to_datetime(data.dataregistro)
+    data.datafinalprevista = pandas.to_datetime(data.datafinalprevista)
+    data.dataenvioresposta = pandas.to_datetime(data.dataenvioresposta)
+
+    print("Boxplot and Scatter Matrix")
+    data['previsto'] = (data.datafinalprevista - data.dataregistro).astype('timedelta64[D]')
+    data['realizado'] = (data.dataenvioresposta - data.dataregistro).astype('timedelta64[D]')
+    data['delta'] = data.previsto - data.realizado
+
+    return data
+
--- examples/ouvidoria.py
+++ examples/ouvidoria.py
@@ -1,36 +1,13 @@
-#from dyrapy import datasets
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
+from dyrapy.datasets import load_ouvidoria
 
 print('Open file...')
-import pandas
-data = pandas.read_csv('../dyrapy/datasets/data/ouvidoria.csv', sep=';', na_values=".")
-
-print('Data head...')
-print(data.head())
-
-print('Data describe..')
-print(data.describe())
-
-print("Recoding categories...")
-data.descricao = data.descricao.astype('category')
-data.sigla = data.sigla.astype('category')
-data.statussolicitacao = data.statussolicitacao.astype('category')
-data.tipofim = data.tipofim.astype('category')
-
-print('Recodind dates..')
-data.data_extracao = pandas.to_datetime(data.data_extracao)
-data.dataregistro = pandas.to_datetime(data.dataregistro)
-data.datafinalprevista = pandas.to_datetime(data.datafinalprevista)
-data.dataenvioresposta = pandas.to_datetime(data.dataenvioresposta)
-
-print("Boxplot and Scatter Matrix")
-data['previsto'] = (data.datafinalprevista - data.dataregistro).astype('timedelta64[D]')
-data['realizado'] = (data.dataenvioresposta - data.dataregistro).astype('timedelta64[D]')
-data['delta'] = data.previsto - data.realizado
+data = load_ouvidoria()
 
 bp = data.boxplot()
-bp.set_title('Número de dias previstos e realizados, com variação entre previsto e realizado (delta).' )
-from pandas.plotting import scatter_matrix
+bp.set_title('Número de dias previstos e realizados, com variação entre previsto e realizado (delta).')
+
 scatter_matrix(data[['previsto', 'realizado', 'delta']])
 
-import matplotlib.pyplot as plt
 plt.show()
