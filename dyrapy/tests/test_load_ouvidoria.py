871304c9d468603a31802f91261dcbd3d130a4cf
Marco Mangan <marco.mangan@gmail.com>
2020-12-16 21:43:56 -0300

    #10

--- dyrapy/tests/test_load_ouvidoria.py
+++ dyrapy/tests/test_load_ouvidoria.py
@@ -1,4 +1,6 @@
 from dyrapy.datasets import load_ouvidoria
-data = load_ouvidoria()
 
-assert data is not None
+
+def test_load_ouvidoria():
+    data = load_ouvidoria()
+    assert data is not None
