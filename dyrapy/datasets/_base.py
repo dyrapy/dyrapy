4be6ee14f4ed8e4400ea95471a487b273dd91209
Marco Mangan <marco.mangan@gmail.com>
2020-12-24 17:52:16 -0300

    #15

--- dyrapy/datasets/_base.py
+++ dyrapy/datasets/_base.py
@@ -4,6 +4,7 @@
 """
 Base IO code for all datasets
 """
+import logging
 
 import pandas
 from os.path import dirname, join
@@ -26,25 +27,25 @@ def load_ouvidoria():
 
     data = pandas.read_csv(ouvidoria_csv_filename, sep=';', na_values=".")
 
-    print('Data head...')
-    print(data.head())
+    logging.info('Data head...')
+    logging.info(data.head())
 
-    print('Data describe..')
-    print(data.describe())
+    logging.info('Data describe..')
+    logging.info(data.describe())
 
-    print("Recoding categories...")
+    logging.info("Recoding categories...")
     data.descricao = data.descricao.astype('category')
     data.sigla = data.sigla.astype('category')
     data.statussolicitacao = data.statussolicitacao.astype('category')
     data.tipofim = data.tipofim.astype('category')
 
-    print('Recodind dates..')
+    logging.info('Recodind dates..')
     data.data_extracao = pandas.to_datetime(data.data_extracao)
     data.dataregistro = pandas.to_datetime(data.dataregistro)
     data.datafinalprevista = pandas.to_datetime(data.datafinalprevista)
     data.dataenvioresposta = pandas.to_datetime(data.dataenvioresposta)
 
-    print("Boxplot and Scatter Matrix")
+    logging.info("Boxplot and Scatter Matrix")
     data['previsto'] = (data.datafinalprevista - data.dataregistro).astype('timedelta64[D]')
     data['realizado'] = (data.dataenvioresposta - data.dataregistro).astype('timedelta64[D]')
     data['delta'] = data.previsto - data.realizado
@@ -66,17 +67,16 @@ def load_escolas():
     data_cadastro = pandas.read_csv(cadastro_csv_filename, sep=';', na_values=".")
     data_matriculas = pandas.read_csv(matriculas_csv_filename, sep=';', na_values=".")
 
-    print('Data head...')
-    print(data_cadastro.head())
-    print()
-    print(data_matriculas.head())
+    logging.info('Data head...')
+    logging.info(data_cadastro.head())
+    logging.info(data_matriculas.head())
 
-    print('Data describe..')
-    print(data_cadastro.describe())
-    print()
-    print(data_matriculas.describe())
+    logging.info('Data describe..')
+    logging.info(data_cadastro.describe())
+    logging.info()
+    logging.info(data_matriculas.describe())
 
-    print('Recodind dates..')
+    logging.info('Recodind dates..')
     data_cadastro.data_extracao = pandas.to_datetime(data_cadastro.data_extracao)
     data_matriculas.data_extracao = pandas.to_datetime(data_matriculas.data_extracao)
 
